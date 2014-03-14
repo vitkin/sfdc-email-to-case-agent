a98f6d7e1c01b6a7d29294b54c514060ce512839
Victor Itkin <victor.itkin@gmail.com>
2014-03-14 15:15:51 +0200

    Rename main folder and relocate scripts to bin

--- src/main/scripts/email2case.bat
+++ src/main/scripts/email2case.bat
@@ -2,15 +2,21 @@
 ::
 :: Script to run the agent directly not as a daemon.
 ::
-SET CONFIG_FILE=config\sfdcConfig.xml
+SET CONFIG_FILE=config/sfdcConfig.xml
 SET LOG4J_CONFIG_FILE=config/log4j.xml
 SET LOG_DIR=log
 SET LOG_FILE=%LOG_DIR%\email2case.log
 
-SET JAVA_HOME=C:\PROGRA~1\Java\jdk1.7.0_45
-SET JAVA_OPTS=-server -Xmx128m -Dlog4j.configuration=file:%LOG4J_CONFIG_FILE%
+SET JAVA_HOME=C:\PROGRA~1\Java\jdk1.7.0_51
 
-SET EMAIL2CASE_HOME=%~dp0
+SET JAVA_OPTS=-server ^
+              -Xmx128m ^
+              -Dlog4j.configuration=file:%LOG4J_CONFIG_FILE% ^
+              -Dlog4j.debug ^
+              -Dmail.imap.connectiontimeout=900000 ^
+              -Dmail.imap.timeout=750000
+
+SET EMAIL2CASE_HOME=%~dp0\..
 
 ECHO SFDC Email-to-Case Agent v${project.version}
 CD %EMAIL2CASE_HOME%
--- src/main/scripts/email2case.sh
+++ src/main/scripts/email2case.sh
@@ -7,10 +7,16 @@ LOG4J_CONFIG_FILE=config/log4j.xml
 LOG_DIR=log
 LOG_FILE=$LOG_DIR/email2case.log
 
-JAVA_HOME=/usr/java/jdk1.7.0_45
-JAVA_OPTS="-server -Xmx128m -Dlog4j.configuration=file:$LOG4J_CONFIG_FILE"
+JAVA_HOME=/usr/java/jdk1.7.0_51
 
-EMAIL2CASE_HOME=$(dirname "${0}")
+JAVA_OPTS="-server \
+           -Xmx128m \
+           -Dlog4j.configuration=file:$LOG4J_CONFIG_FILE \
+           -Dlog4j.debug \
+           -Dmail.imap.connectiontimeout=900000 \
+           -Dmail.imap.timeout=750000"
+
+EMAIL2CASE_HOME=$(dirname "${0}")/..
 
 echo SFDC Email-to-Case Agent v${project.version}
 cd $EMAIL2CASE_HOME
